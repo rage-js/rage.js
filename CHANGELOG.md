c5529e4add79a71a8091d906c25f4cce98d7f63d
Maghish <mail4mithran@gmail.com>
2024-10-21 20:47:39 +0530

    v0.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# rage.js
+
+## 0.0.1
+
+- ### Inital Commit
--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ import MongoDBCollection from "./classes/MongoDB/MongoDBCollection";
 import MongoDBSchema from "./classes/MongoDB/MongoDBSchema";
 
 export { App, MongoDBTookKit, MongoDBCollection, MongoDBSchema };
-export * from "./main";
+export type * from "./main";
